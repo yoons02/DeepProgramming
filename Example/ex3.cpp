799e659a755a8f612c1a2fcb5bc4057f2f805ba3
yoons02 <kys5037k@naver.com>
2022-09-20 13:43:03 +0900

    example_9/19_2

--- Example/ex3.cpp
+++ Example/ex3.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Car{
 public:
     int speed;
     int gear;
-    char color;
+    string color;
 
     int speedUp();
     int speedDown();
     int gearUp();
     int gearDown();
-    char colorTrans();
+    string colorTrans();
 };
 
 int Car::speedUp(){
@@ -30,20 +31,21 @@ int Car::gearDown(){
     return gear-1;
 }
 
-char Car::colorTrans(){
-    if (color == 'Black'){
-        return 'Gray';
+string Car::colorTrans(){
+    if (color == "Black"){
+        color = "Gray";
     }
-    else if (color == 'Gray'){
-        return 'Black';
+    else if (color == "Gray"){
+        color = "Black";
     }
+    return color; 
 }
 
 int main(){
     Car rec;
     rec.speed = 180;
     rec.gear = 4;
-    rec.color = 'Gray';
+    rec.color = "Gray";
 
     cout << "현재 속도는 " << rec.speedUp() << ", 기어는 " << rec.gearUp() << ", 현재 자동차 색상은 " << rec.colorTrans() << endl;
     }
